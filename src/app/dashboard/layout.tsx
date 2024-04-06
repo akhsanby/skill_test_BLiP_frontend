5968e03d231e61de5c9dc98680982245740088d9
akhsanby <akhsanby@gmail.com>
2024-04-06 07:50:28 +0700

    initial route

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,7 @@
+import { Metadata } from "next";
+import Page from "@/app/dashboard/page";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
+export default Page;
---
+++ src/app/order/layout.tsx
@@ -0,0 +1,7 @@
+import { Metadata } from "next";
+import Page from "@/app/order/page";
+
+export const metadata: Metadata = {
+  title: 'Order'
+};
+export default Page;
